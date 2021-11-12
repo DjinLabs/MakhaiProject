1d58aef26e1dc2d0bf076d4d21ec2bb53a379613
Alber <4lb3rduris@gmail.com>
2021-11-12 09:24:24 +0100

    Refactor of Tribes.py and changes in main

--- Tribes.py
+++ Tribes.py
@@ -1,5 +1,27 @@
-TRIBE_NAMES = ['Raperos', 'Punks', 'Otakus', 'Jipis']
+import streamlit as st
+from random import sample
 
+
+class TribeManager:
+    def __init__(self):
+        self.tribes = []
+        self.tribe_names = []
+        self._slots = ['A', 'B', 'C', 'D']
+
+    def create_tribes(self, tribe_names):
+        self.tribe_names = tribe_names
+        for tribe_name in self.tribe_names:
+            self.tribes.append(Tribe(tribe_name))
+
+    def get_slots(self):
+        slots = sample(self._slots, len(self._slots))
+        for s, t in zip(slots, self.tribes):
+            t.slot = s
+        self.tribes.sort(key=lambda x: x.slot)
+
+    @property
+    def slots(self):
+        return self._slots
 
 class Tribe:
     def __init__(self, name):
@@ -16,4 +38,8 @@ class Tribe:
 
     @slot.setter
     def slot(self, value):
-        self.slot = value
+        self._slot = value
+
+
+
+
--- fight_simulator.py
+++ fight_simulator.py
@@ -1,40 +1,30 @@
 import streamlit as st
 import random
-from random import sample
 
 import Tribes as Tribes
+from Tribes import TribeManager
 from Tribes import Tribe
 
-random.seed('297145941b593410080ad664a30f7b4b371a6a0fa497b365b836c8f9252da782')
+st.set_page_config(layout='wide')
 
 # GLOBAL VARIABLES
+TRIBE_NAMES_PLACEHOLDER = ['Raperos', 'Punks', 'Otakus', 'Jipis']
 
-
-
-### Functions
-def get_slots(tribes):
-    slots = sample(['A', 'B', 'C', 'D'], 4)
-    tribes_dict = {}
-    for s, t in zip(slots, tribes):
-        tribes_dict[t] = {'slot': s}
-
-    return dict(sorted(tribes_dict.items(), key=lambda item: item[1]['slot']))  # Sort by slot
-
+random.seed('297145941b593410080ad664a30f7b4b371a6a0fa497b365b836c8f9252da782')
 
 st.title('TeamFight Tokens')
 
 st.subheader('Fight simulator')
 
-# Create Tribes #@TODO: Mover to Tribes.py
-TRIBES = []
-for tribe_name in Tribes.TRIBE_NAMES:
-    TRIBES.append(Tribe(tribe_name))
-
-tribes_text_input = [st.text_input(f'Tribe {i + 1}', TRIBES[i].name) for i, tribe in enumerate(TRIBES)]
+# Create Tribes from Text Input with  Placeholder pre-fill
+tribe_manager = TribeManager()
+cols = st.columns(len(tribe_manager.slots))
+with cols[0]:
+    tribes_names_input = [st.text_input(f'Tribe {i + 1}', TRIBE_NAMES_PLACEHOLDER[i])
+                          for i, tribe in enumerate(TRIBE_NAMES_PLACEHOLDER)]
+    tribe_manager.create_tribes(tribes_names_input)
 
 if st.button('Get slots'):
-    tribes_dict = get_slots(Tribes.TRIBE_NAMES)
-
-    for k, v in tribes_dict.items():
-        st.write(
-            f'Tribe "{k}" is on slot {v["slot"]}')
+    tribe_manager.get_slots()
+    cols = st.columns(len(tribe_manager.slots))
+    _ = [cols[i].write(f'Tribe "{v.name}" is on slot {v.slot}') for i, v in enumerate(tribe_manager.tribes)]
